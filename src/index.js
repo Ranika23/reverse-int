f940c5d4031b440d6fad3d8e14ed9869443647e6
Ranika23 <bveronikal@gmail.com>
2023-08-08 15:03:12 +0300

    docs: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const str = String(n);
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if ('0123456789'.includes(str[i]) == true) result = str[i] + result
+  }
+  return Number(result);
 }
